acd944a77a808640d205a3556234b01dd526a479
Marty <juhu>
2020-08-23 20:46:46 +0200

    point to monero rebase

--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -5,11 +5,11 @@ git submodule update --init --recursive
 
 # update haven-web-cpp
 cd ./external/haven-web-cpp
-git checkout #tags/v0.3.3
+git checkout master #tags/v0.3.3
 git pull ##--ff-only origin tags/v0.3.3
 
 # update haven-offshore
 cd ./external/haven-offshore
-git checkout master
+git checkout monero-v0.16
 git pull #--ff-only origin tags/v0.16.0.1
 cd ../../../../
