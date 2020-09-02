1961d2d6078a6e1c9bbaa7921800ccd6ca1f8bd8
Marty <juhu>
2020-09-02 16:06:33 +0200

    add rpc login ( required for web )

--- bin/start_daemon.sh
+++ bin/start_daemon.sh
@@ -1,1 +1,1 @@
-./external/haven-web-cpp/external/haven-offshore/monero/build/Darwin/release-v0.16/release/bin/havend --stagenet --offline --fixed-difficulty=50
+./external/haven-web-cpp/external/haven-offshore/monero/build/Darwin/release-v0.16/release/bin/havend --stagenet --offline --fixed-difficulty=50 --rpc-access-control-origin=http://localhost:9100 --rpc-login=super:super
