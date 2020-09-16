cdf560af23cc1f5d7c98544b46895962dd70596a
Marty <juhu>
2020-09-16 15:20:26 +0200

    Create HavenTxType.d.ts

---
+++ src/main/js/wallet/model/HavenTxType.d.ts
@@ -0,0 +1,14 @@
+export = HavenTxType;
+/**
+ * Enumerates Haven Tx Types.
+ *
+ * @hideconstructor
+ */
+declare class HavenTxType {
+}
+declare namespace HavenTxType {
+    const CLASSIC: number;
+    const ONSHORE: number;
+    const OFFSHORE: number;
+    const OFFSHORE_TO_OFFSHORE: number;
+}
