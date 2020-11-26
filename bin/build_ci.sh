5888b59f64f1455bee0e0f7a90fbdb31ede88c0b
goshiz <goshiz@goshiz.io>
2020-11-26 18:06:55 +0100

    Init CI/CD on Github

---
+++ bin/build_ci.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# check for emscripten
+[ -z ${EMSDK} ] \
+  && {
+    echo "Missing EMSDK Environment variable.  Did you remember to run 'source /path/to/emsdk/emsdk_env.sh' ?"
+    [ "$(basename $0)" = "bash" ] \
+    || { 
+      echo "Terminating..."
+      exit 1
+    }
+  }
+
+# update submodules
+./bin/update_submodules.sh || exit 1
+
+# build haven offshore translations directory
+cd ./external/haven-web-cpp/external/haven-offshore || exit 1
+# HOST_NCORES=$(nproc 2>/dev/null || shell nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || echo 1)
+./build-haven.sh -j8		# don't exit because this will build translations directory even if build fails
+cd ../../../../ || exit 1
+
+# build boost
+./bin/build_boost_emscripten.sh || exit 1
+
+# build openssl
+./bin/build_openssl_emscripten.sh || exit 1
+
+# build webassembly for distribution
+./bin/build_wasm_emscripten.sh || exit 1
+
+# build web worker
+npm install || exit 1
+./bin/build_web_worker.sh || exit 1
+
+# build browser tests
+#./bin/build_browser_tests.sh || exit 1
--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -9,7 +9,16 @@ git checkout master #tags/v0.3.3
 git pull ##--ff-only origin tags/v0.3.3
 
 # update haven-offshore
+branchname=$(git branch --show-current)
+tagname=$(git tag --points-at HEAD)
+if [ -z "$branchname" ]
+then
+    git checkout tags/$tagname
+else
+    git checkout $branchname
+fi
+
 cd ./external/haven-offshore
-git checkout monero-v0.16
+
 git pull #--ff-only origin tags/v0.16.0.1
 cd ../../../../
