25be2c6cd62e42905ed1ab4664ad15661a836114
Marty <51510876+MartyHav@users.noreply.github.com>
2021-03-13 19:04:14 +0100

    Revert "switch to haven-main repo"
    This reverts commit e3d65e406815bcc9a53a2a1bf5fa8a1defcb4969.

--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -8,12 +8,12 @@ cd ./external/haven-web-cpp
 git checkout feature/xassets #tags/v0.3.3
 git pull ##--ff-only origin tags/v0.3.3
 
-# update haven-main
+# update haven-offshore
 
 branchname=$(git --git-dir ../../.git branch --show-current)
 tagname=$(git --git-dir ../../.git tag --points-at HEAD)
 
-cd ./external/haven-main
+cd ./external/haven-offshore
 
 if [ -z "$branchname" ]
 then
