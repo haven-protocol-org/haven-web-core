130c0ca1fd5826eaaa32139e19d773c3ec45a951
Marty <51510876+MartyHav@users.noreply.github.com>
2021-03-27 22:13:06 +0100

    update version

--- src/main/js/common/LibraryUtils.d.ts
+++ src/main/js/common/LibraryUtils.d.ts
@@ -18,7 +18,7 @@ declare class LibraryUtils {
     /**
      * Load the WebAssembly keys module with caching.
      */
-    static loadKeysModule(): unknown;
+    static loadKeysModule(): Promise<any>;
     /**
      * Load the WebAssembly core module with caching.
      *
@@ -26,7 +26,7 @@ declare class LibraryUtils {
      *
      * TODO: this is separate static function from loadKeysModule() because webpack cannot bundle WebWorker using runtime param for conditional import
      */
-    static loadCoreModule(): unknown;
+    static loadCoreModule(): Promise<any>;
     /**
      * Private helper to initialize the wasm module with data structures to synchronize access.
      */
@@ -38,7 +38,7 @@ declare class LibraryUtils {
      * @param {string} fnId - unique identifier for the function
      * @param {function} fn - function to inform if unauthorized requests should be rejected
      */
-    static setRejectUnauthorizedFn(fnId: string, fn: {}): void;
+    static setRejectUnauthorizedFn(fnId: string, fn: Function): void;
     /**
      * Indicate if unauthorized requests should be rejected.
      *
@@ -46,10 +46,10 @@ declare class LibraryUtils {
      */
     static isRejectUnauthorized(fnId: string): any;
     /**
-     * Set the path to load MoneroWebWorker.dist.js when running this library in
-     * a web worker (defaults to "/MoneroWebWorker.dist.js").
+     * Set the path to load HsavenWebWorker.dist.js when running this library in
+     * a web worker (defaults to "/HavenWebWorker1.2.36.dist.js").
      *
-     * @param {string} workerDistPath - path to load MoneroWebWorker.dist.js
+     * @param {string} workerDistPath - path to load HavenWebWorker1.2.36.dist.js
      */
     static setWorkerDistPath(workerDistPath: string): void;
     /**
@@ -57,7 +57,7 @@ declare class LibraryUtils {
      *
      * @return {Worker} a worker to share among wallet instances
      */
-    static getWorker(): any;
+    static getWorker(): Worker;
     /**
      * Invoke a web worker function and get the result with error handling.
      *
@@ -66,7 +66,7 @@ declare class LibraryUtils {
      * @param {Object[]} args are function arguments to invoke with
      * @return {Promise} resolves with response payload from the worker or an error
      */
-    static invokeWorker(objectId: any, fnName: string, args: any[]): any;
+    static invokeWorker(objectId: any, fnName: string, args: any[]): Promise<any>;
 }
 declare namespace LibraryUtils {
     export const WORKER_DIST_PATH_DEFAULT: string;
