f88b64cf78558211ff3b21146b0eeb82ba6630ed
Marty <51510876+MartyHav@users.noreply.github.com>
2021-05-18 16:32:25 +0200

    adjust web worker build

--- webpack.web-worker.js
+++ webpack.web-worker.js
@@ -5,6 +5,10 @@ const configBase = require("./webpack.base.js");
 const VERSION = require('./package.json').version
 let configMoneroWebWorker = Object.assign({}, configBase, {
   target: "webworker",
+  node: {
+    __filename: false,
+    __dirname: false,
+  },
   name: "Haven web worker config",
   entry: "./src/main/js/common/MoneroWebWorker.js",
   output: {
