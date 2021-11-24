4865cffae7356d95b6e992afff1b45477c83d99e
Marty <51510876+MartyHav@users.noreply.github.com>
2021-11-24 16:55:01 +0100

    linux build fix

--- bin/build_web_worker.sh
+++ bin/build_web_worker.sh
@@ -5,7 +5,11 @@
 # get current package version
 NPM_VERSION=$(node -p -e "require('./package.json').version")
 #rename webworker reference in LibraryUtils
-sed -i "" "s/HavenWebWorker[0-9\.]*/HavenWebWorker${NPM_VERSION}./g" ./src/main/js/common/LibraryUtils.js 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i "" "s/HavenWebWorker[0-9\.]*/HavenWebWorker${NPM_VERSION}./g" ./src/main/js/common/LibraryUtils.js
+else
+  sed -i "s/HavenWebWorker[0-9\.]*/HavenWebWorker${NPM_VERSION}./g" ./src/main/js/common/LibraryUtils.js
+fi
 # build web app dependencies -> adds current package version on its own
 npm run build_worker
 
