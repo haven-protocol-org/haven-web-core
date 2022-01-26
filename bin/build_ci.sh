b9b78bb945dd43e3d0bfa52069006feb8f6a6055
Marty <51510876+MartyHav@users.noreply.github.com>
2022-01-26 13:55:08 +0100

    switch to haven_main develop branch

--- bin/build_ci.sh
+++ bin/build_ci.sh
@@ -16,7 +16,7 @@
 
 # build haven offshore translations directory
 cd ./external/haven-web-cpp/external/haven || exit 1
-git checkout per_output_unlock
+git checkout develop
 git pull
 mkdir -p build/release
 cd build/release
