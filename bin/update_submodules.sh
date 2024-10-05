8b134448d42e6b9577e067f1bba656a15ea50418
borgvin <borgvin@havenprotocol.org>
2024-10-05 09:18:59 +0100

    temporary hack to fix build after difficulty bug fix

--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -2,3 +2,6 @@
 
 # initialize submodules recursively
 git submodule update --init --recursive
+
+# hack for building after difficulty drift fix
+sed -i 's/long double/double/g' external/haven-web-cpp/external/haven/src/cryptonote_basic/difficulty.cpp
